ed2bdfe5259fef3afc240ad34cd9f6ec9c58074d
Florencia Carvajal <florcar1998@gmail.com>
2024-09-19 11:20:42 -0300

    correccion de test carrito

--- src/main/java/com/tallerwebi/dominio/entidades/Carrito.java
+++ src/main/java/com/tallerwebi/dominio/entidades/Carrito.java
@@ -7,7 +7,9 @@ public class Carrito {
     private List<Libro> libros = new ArrayList<>();
 
     public void agregarLibro(Libro libro) {
-        libros.add(libro);
+       if(!libros.contains(libro)) {
+           libros.add(libro);
+       }
     }
 
     public List<Libro> getLibros() {
